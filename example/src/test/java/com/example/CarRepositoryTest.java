482074b3e5f488e423ca88df83b07fe36ef8d578
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-12-19 17:20:05 -0500

    Tests failing due to implementation logic

--- example/src/test/java/com/example/CarRepositoryTest.java
+++ example/src/test/java/com/example/CarRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static com.example.RoofType.HARDTOP;
 import static org.junit.Assert.assertEquals;
 
 public class CarRepositoryTest {
@@ -19,11 +20,13 @@ public class CarRepositoryTest {
     @Test
     public void testFindByColor() throws Exception {
         Car redCar = new Car();
+        redCar.setRoofType(HARDTOP);
         redCar.setColor("red");
         Car savedRedCar = carRepository.save(redCar);
 
         Car blueCar = new Car();
         blueCar.setColor("blue");
+        blueCar.setRoofType(HARDTOP);
         Car savedBlueCar = carRepository.save(blueCar);
 
         List<Car> redCars = carRepository.findByColor("red");
